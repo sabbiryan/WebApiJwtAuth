2832311a2786d72a15406ea5d4aa1395d05c9096
Md. Sabbir Ahamed <sabbiryan@gmail.com>
2015-12-03 02:30:49 +0600

    Assigning claims to the user on the fly

---
+++ Source/WebApiAuth/WebApiAuth.Server/Controllers/ClaimsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace WebApiAuth.Server.Controllers
+{
+    [RoutePrefix("api/claims")]
+    public class ClaimsController : BaseApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult GetClaims()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var claims = identity.Claims.Select(c => new
+                {
+                    subject = c.Subject.Name,
+                    type = c.Type,
+                    value = c.Value
+                });
+
+                return Ok(claims);
+            }
+
+            return NotFound();
+        }
+
+    }
+}
